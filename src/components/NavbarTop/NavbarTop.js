8713245cdd12bea7610f612a62e88f6b3f8902ee
monukr14 <monukr2012@gmail.com>
2020-11-17 14:28:17 +0530

    update

--- src/components/NavbarTop/NavbarTop.js
+++ src/components/NavbarTop/NavbarTop.js
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
 import { Button, Navbar, Nav } from 'react-bootstrap';
+import './NavbarTop.css'
 export default class NavbarTop extends Component {
   render() {
     return (
       <div>
-          <Navbar bg="dark" variant="dark">
+          <Navbar >
         <div className="container-fluid">
         
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">Instadriver</Navbar.Brand>
           <Nav className="mr-auto"></Nav>
-          <Button variant="outline-info" className="mr-sm-2">
+          <Button variant="outline-info" className="mr-sm-2 nav-btn-stl">
             Search
           </Button>
-          <Button variant="outline-info">Search</Button>
+          <Button className="nav-btn-stl" variant="outline-info">Search</Button>
        
         </div>
         </Navbar>
