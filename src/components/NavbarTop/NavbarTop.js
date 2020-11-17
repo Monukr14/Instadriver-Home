d01b4a180c44bef45456e8b3501629f3546c46f3
monukr14 <monukr2012@gmail.com>
2020-11-17 14:34:49 +0530

    update

--- src/components/NavbarTop/NavbarTop.js
+++ src/components/NavbarTop/NavbarTop.js
@@ -5,15 +5,15 @@ export default class NavbarTop extends Component {
   render() {
     return (
       <div>
-          <Navbar >
+          <Navbar  sticky="top" >
         <div className="container-fluid">
         
           <Navbar.Brand href="#home">Instadriver</Navbar.Brand>
           <Nav className="mr-auto"></Nav>
           <Button variant="outline-info" className="mr-sm-2 nav-btn-stl">
-            Search
+            Jobs
           </Button>
-          <Button className="nav-btn-stl" variant="outline-info">Search</Button>
+          <Button className="nav-btn-stl" variant="outline-info">Sign in</Button>
        
         </div>
         </Navbar>
